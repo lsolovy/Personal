70024006dcb20697befa97314723310cc4bba721
lsolovy <solovy.louis@gmail.com>
2020-06-03 20:14:08 -0500

    added a end screen that prompts the user and also shows the final score

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,5 +1,5 @@
 // Basic code done from FamTrinli on youtube.com - https://www.youtube.com/watch?v=OBBrp43TX3A
-// The updates that I have added...reformated code/commentation/
+// The updates that I have added...reformated code/added a welcome screen and end screen/implemented a score/implemented a state machine
 #include <SFML/Graphics.hpp>
 #include <time.h>
 #include <string>
@@ -13,7 +13,7 @@ int size = 16;
 int w = size * N;
 int h = size * M;
 
-int dir, num = 3;
+int dir, num = 2;
 
 struct Snake {
     int x, y;
@@ -22,6 +22,7 @@ struct Snake {
 struct Fruit {
     int x, y;
 } f;
+
 
 void Tick() {
     for (int i = num; i > 0; --i) {
@@ -33,6 +34,7 @@ void Tick() {
     if (dir == 1) { s[0].x -= 1; }
     if (dir == 2) { s[0].x += 1; }
     if (dir == 3) { s[0].y -= 1; }
+
     // if the snake touches the food it will get added to the end of the snake
     // new food will be generated
     if ((s[0].x == f.x) && (s[0].y == f.y)) {
@@ -46,13 +48,12 @@ void Tick() {
     if (s[0].y > M) { s[0].y = 0; }
     if (s[0].y < 0) { s[0].y = M; }
 
-    for (int i = 1; i < num; i++) {
-        if (s[0].x == s[i].x && s[0].y == s[i].y) { num = i; }
-    }
+
+
 }
 
 int main() {
-
+    int maxnum = 0;
     srand(time(0));
 
     RenderWindow window(VideoMode(w, h), "Snake Game!");
@@ -73,6 +74,10 @@ int main() {
     welcometext.setFont(font);
     Text scoretext;
     scoretext.setFont(font);
+    Text gameovertext;
+    gameovertext.setFont(font);
+    Text scoretext2;
+    scoretext2.setFont(font);
 
     Button btn1("Easy", {100,50},20,Color::Green,Color::Black);
     btn1.setFont(font);
@@ -83,6 +88,12 @@ int main() {
     Button btn3("Hard", {100,50},20,Color::Green,Color::Black);
     btn3.setFont(font);
     btn3.setPosition({350,200});
+    Button btn4("Restart", {100,50},15,Color::Green,Color::Black);
+    btn4.setFont(font);
+    btn4.setPosition({50,200});
+    Button btn5("Quit", {100,50},15,Color::Green,Color::Black);
+    btn5.setFont(font);
+    btn5.setPosition({350,200});
 
     Clock clock;
     float timer = 0, delay = 0.1;
@@ -98,7 +109,7 @@ int main() {
     while (window.isOpen()) {
         if (Game == Welcome_State) {
             Event e;
-
+            maxnum = 0;
             while (window.pollEvent(e)) {
                 if (e.type == Event::Closed) {
                     window.close();
@@ -146,7 +157,7 @@ int main() {
                     }
                 }
             }
-
+            window.clear();
             welcometext.setString("Welcome to Snake!");
             welcometext.setCharacterSize(22);
             welcometext.setFillColor(sf::Color::Green);
@@ -167,19 +178,28 @@ int main() {
                     window.close();
                 }
             }
-            // whe the arrows are pressed the snake will move in that direction
+
             if (Keyboard::isKeyPressed(Keyboard::Left)) { dir = 1; }
             if (Keyboard::isKeyPressed(Keyboard::Right)) { dir = 2; }
             if (Keyboard::isKeyPressed(Keyboard::Up)) { dir = 3; }
-            if (Keyboard::isKeyPressed(Keyboard::Down)) { dir = 0; }
+            if (Keyboard::isKeyPressed(Keyboard::Down)) {dir = 0;}
 
             if (timer > delay) {
                 timer = 0;
                 Tick();
+
+            }
+            for (int i = 1; i < num; i++) {
+                if (s[0].x == s[i].x && s[0].y == s[i].y) {
+                    num = i;
+                    Game = End_State;
+                }
             }
 
+            if(num > maxnum){
+                maxnum = num;
+            }
             ////// draw  ///////
-
             // draws the background
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
@@ -196,11 +216,62 @@ int main() {
             sprite3.setPosition(f.x * size, f.y * size);
             window.draw(sprite3);
 
+
             scoretext.setString("Score: " + to_string(num));
             scoretext.setCharacterSize(12);
             scoretext.setFillColor(sf::Color::Red);
             scoretext.setPosition(400,0);
             window.draw(scoretext);
+            window.display();
+        }
+        else{
+            Event e;
+            while (window.pollEvent(e)) {
+                if (e.type == Event::Closed) {
+                    window.close();
+                }
+            }
+            if(e.type == Event::MouseMoved){
+                if(btn4.isMouseOver(window)){
+                    btn4.setBackColor(Color::White);
+                }
+                else{
+                    btn4.setBackColor(Color::Green);
+                }
+            }
+            if(e.type == Event::MouseButtonPressed){
+                if(btn4.isMouseOver(window)){
+                    Game = Welcome_State;
+
+                }
+            }
+            if(e.type == Event::MouseMoved){
+                if(btn5.isMouseOver(window)){
+                    btn5.setBackColor(Color::White);
+                }
+                else{
+                    btn5.setBackColor(Color::Green);
+                }
+            }
+            if(e.type == Event::MouseButtonPressed){
+                if(btn5.isMouseOver(window)){
+                    window.close();
+
+                }
+            }
+            window.clear();
+            gameovertext.setString("GAME OVER");
+            gameovertext.setCharacterSize(45);
+            gameovertext.setPosition(100,100);
+            gameovertext.setFillColor(sf::Color::Green);
+            scoretext2.setString("Score = " + to_string(maxnum));
+            scoretext2.setCharacterSize(25);
+            scoretext2.setFillColor(sf::Color::Green);
+            scoretext2.setPosition(170,165);
+            window.draw(gameovertext);
+            window.draw(scoretext2);
+            btn4.drawTo(window);
+            btn5.drawTo(window);
             window.display();
         }
     }
