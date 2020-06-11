30239fd5616ceb004db13ad93264ff42504092ba
lsolovy <solovy.louis@gmail.com>
2020-06-10 19:44:52 -0500

    snake game

---
+++ Snake/cmake-build-debug/CMakeFiles/Bejeweled.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Bejeweled.exe"
+  "Bejeweled.exe.manifest"
+  "Bejeweled.pdb"
+  "CMakeFiles/Bejeweled.dir/main.cpp.obj"
+  "libBejeweled.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bejeweled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
