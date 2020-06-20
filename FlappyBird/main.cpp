b585f5867a7e51119de98e2c9ad1af4f7a1bdf3b
lsolovy <solovy.louis@gmail.com>
2020-06-20 15:11:52 -0500

    intial commit

--- FlappyBird/main.cpp
+++ FlappyBird/main.cpp
@@ -1,16 +1,26 @@
 
 #include <SFML/Graphics.hpp>
 #include <time.h>
-#include <list>
 #include <cmath>
-#include <SFML/Audio.hpp>
-#include <string>
+
+#include "Button.h"
 
 
 using namespace sf;
 using namespace std;
-const int W = 1200;
-const int H = 800;
+const float W = 1200;
+const float H = 800;
+
+
+void Bird(RenderWindow(&app)){
+    CircleShape circle(50.f);
+    circle.setFillColor(Color(255,0,0));
+    circle.setPosition(200,H/2);
+    circle.setPointCount(100);
+    app.draw(circle);
+    app.display();
+}
+
 
 int main() {
 
@@ -18,7 +28,13 @@ int main() {
 
     RenderWindow app(VideoMode(W, H), "FlappyBird!");
     app.setFramerateLimit(60);
-
+    Font font;
+    if (!font.loadFromFile("../fonts/Gameplay.ttf")) {
+        return EXIT_FAILURE;
+    }
+    Button btn1("Start", {100,50},20,Color::Green,Color::Black);
+    btn1.setFont(font);
+    btn1.setPosition({50,200});
 
     //start of state machine
     enum GameState{
@@ -33,16 +49,47 @@ int main() {
                 if (e.type == Event::Closed) {
                     app.close();
                 }
-                if (e.type == Event::KeyPressed) {
-                    if (e.key.code == Keyboard::Space) {
+                if(e.type == Event::MouseMoved){
+                    if(btn1.isMouseOver(app)){
+                        btn1.setBackColor(Color::White);
+                    }
+                    else{
+                        btn1.setBackColor(Color::Green);
+                    }
+                }
+                if(e.type == Event::MouseButtonPressed){
+                    if(btn1.isMouseOver(app)){
                         Game = Play_State;
                     }
+                }
+            }
+            btn1.drawTo(app);
+            app.display();
+        } else if (Game == Play_State){
+            Event e;
+            while (app.pollEvent(e)) {
+                if (e.type == Event::Closed) {
+                    app.close();
+                }
+                if (e.type == Event::KeyPressed) {
+                    if (e.key.code == Keyboard::Space) {
+                        Game = End_State;
+                    }
+                }
+            }
+            app.clear();
+            Bird(app);
+
+        } else {
+            Event e;
+            while (app.pollEvent(e)) {
+                if (e.type == Event::Closed) {
+                    app.close();
                 }
             }
             app.display();
         }
     }
-
 
     return 0;
 }
