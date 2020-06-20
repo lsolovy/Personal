8d3d1e61165c3f5357dc60ffc9f2a3fd1b300ae5
lsolovy <solovy.louis@gmail.com>
2020-06-20 15:35:26 -0500

    to make sure texture coordinate is working

--- Animation/main.cpp
+++ Animation/main.cpp
@@ -9,9 +9,26 @@ using namespace std;
 
 int main() {
     RenderWindow window(VideoMode(512,512),"Animation",Style::Close | Style::Resize);
-    RectangleShape player(Vector2f(100.0f,100.0f));
+    RectangleShape player(Vector2f(400.0f,400.0f));
+    player.setPosition(56.0f,56.0f);
     Texture playerTexture;
     playerTexture.loadFromFile("images/player.png");
+    player.setTexture(&playerTexture);
+    Vector2u texturesize = playerTexture.getSize();
+    texturesize.x /= 6;
+    texturesize.y /= 4;
+    player.setTextureRect(IntRect(texturesize.x*2, texturesize.y*2, texturesize.x,texturesize.y));
 
+    while(window.isOpen()){
+        Event e;
+        while(window.pollEvent(e)){
+            if(e.type == Event::Closed){
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(player);
+        window.display();
+    }
     return 0;
 }
